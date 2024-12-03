eb1dcaa96e8387c87bf2294cfd06fc7b8505045b
Bharath Mohanraj <110107743+bhmohanr-techie@users.noreply.github.com>
2024-12-02 17:17:06 -0800

    Update Conventions.java

--- spring-core/src/main/java/org/springframework/core/Conventions.java
+++ spring-core/src/main/java/org/springframework/core/Conventions.java
@@ -234,7 +234,7 @@ public final class Conventions {
 				upperCaseNext = true;
 			}
 			else if (upperCaseNext) {
-				result[currPos++] = Character.toUpperCase(Locale.ROOT)(c);
+				result[currPos++] = Character.toUpperCase(c);
 				upperCaseNext = false;
 			}
 			else {
